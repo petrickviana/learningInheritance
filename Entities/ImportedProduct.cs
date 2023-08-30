ba7445de4631075fee305e6aebaf0718ffb536ba
Petrick <petrickviana@hotmail.com>
2023-08-30 09:41:01 -0300

    Exercicio herança e polimorfismo

---
+++ Entities/ImportedProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Herançapolimorfismo.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double customsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsfee) : base(name, price)
+        {
+            customsFee = customsfee;
+        }
+
+
+        public double totalPrice()
+        { 
+            return Price + customsFee;
+        }
+
+        public override string priceTag()
+        {
+            return base.priceTag() + " (Customs fee: $ " + customsFee.ToString("F2", CultureInfo.InvariantCulture)+ ")";
+
+        }
+
+
+    }
+}
---
+++ Entities/UsedProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herançapolimorfismo.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime manufactureDate { get; set; }
+
+        public UsedProduct(string name, double price, DateTime manudate) : base(name, price)
+        {
+            manufactureDate = manudate;
+        }
+
+        public override string priceTag()
+        {
+            return Name+ " (Used) $ " + Price + " (Manufacture date: " + manufactureDate.ToString("dd/MM/yyyy") + ")";
+        }
+
+
+    }
+}
